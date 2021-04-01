300b28d445aebbcdaf42d875b87245c0e14aab89
Cristian  Medina <53955934+agro123@users.noreply.github.com>
2021-04-01 01:18:14 -0500

    Modify: eventoCard

--- components/listarEventos/eventoCard.js
+++ components/listarEventos/eventoCard.js
@@ -16,23 +16,29 @@ export default function EventoCard() {
             <Switch checked={!loading} onChange={onChange} />
             <Skeleton loading={loading} active>
                 <Card
-                    style={{ width: 250, height: 430, borderRadius: '20px' }}
+                    style={{
+                        width: 200, 
+                        height: 350, 
+                        borderRadius: '20px',
+                        padding: '10px',
+                        border: '1px solid rgba(59, 66, 72, 0.3)'
+                    }}
                     actions={[
                         <DeleteOutlined key="delete" />,
                         <EditOutlined key="edit" />,
                     ]}
                     hoverable
-                    cover={<img alt="example" src="https://recasens.com/wp-content/uploads/2017/02/r_095_pvc_1.jpg" />}
-                    title="title"
+                    cover={<img alt="example"
+                        src="https://recasens.com/wp-content/uploads/2017/02/r_095_pvc_1.jpg" />}
+                    title="TITULO DEL EVENTO"
                 >
-
                     <div className="eventoCardLeyenda">
                         <div className="eventoCardFecha">
                             <div>20</div>
                             <div>Septiembre</div>
                         </div>
                         <div className="eventoCardDescripcion">
-                            <p>Descripcion:<br/></p>
+                            <p>Descripción: 	<br /></p>
                         </div>
                     </div>
                 </Card>
