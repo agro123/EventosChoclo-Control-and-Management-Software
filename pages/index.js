321f46b456a5a92fdb984ce3c66a5c4ea19f6751
Cristian  Medina <53955934+agro123@users.noreply.github.com>
2021-04-08 10:18:48 -0500

    modify: layout con link

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
+import Link from 'next/link'
+
 export default function Home() {
   return (
     <>
     <h1>Pagina de inicio</h1>
     <ul>
-      <li><a href="/eventosAdmin/listarEventos" >lista de Eventos</a></li>
+      <li><Link href="/eventosAdmin/listarEventos"><a  >lista de Eventos</a></ Link></li>
     </ul>
     </>
   )
