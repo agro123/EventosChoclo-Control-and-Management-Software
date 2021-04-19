3792d238efa151156af66ca4a889450c0db10167
Cristian  Medina <53955934+agro123@users.noreply.github.com>
2021-04-19 12:04:03 -0500

    solucion de conflictos #5

--- pages/_app.js
+++ pages/_app.js
@@ -1,29 +1,28 @@
-import "../styles/globals.css";
-import "../styles/Home.css";
-import "../styles/constants.css";
+import '../styles/globals.css';
+import '../styles/Home.css';
+import '../styles/constants.css';
 import '../styles/listarEventosAdmin.css';
 import '../styles/antdMod.css';
 import 'antd/dist/antd.css';
 import "../styles/crearEventos/eventos.css";
 import "../styles/crearEventos/cardDate.css";
-import Layout from "../components/constants/layout";
-import { ConfigProvider } from "antd";
-import es_ES from "antd/lib/locale/es_ES";
-import Head from "next/head";
+import Layout from '../components/constants/layout';
+import { ConfigProvider } from 'antd';
+import es_ES from 'antd/lib/locale/es_ES';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <>
-      <Head>
-        <title> Eventos Choclo </title>
-      </Head>
-      <ConfigProvider locale={es_ES}>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </ConfigProvider>
+
+    return <>
+        <Head>
+            <title > Eventos Choclo </title> 
+        </Head>
+        <ConfigProvider locale={es_ES} >
+            <Layout >
+                <Component {...pageProps} />
+            </Layout>
+        </ConfigProvider> 
     </>
-  );
 }
 
-export default MyApp;
+export default MyApp
