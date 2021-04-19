07b98785b0ef7f2a392c0be6a54247a83edea099
Cristian  Medina <53955934+agro123@users.noreply.github.com>
2021-04-19 14:49:46 -0500

    modify: eventos.css y deteButton.test

--- test/listar-eliminarEventos/deleteButton.test.js
+++ test/listar-eliminarEventos/deleteButton.test.js
@@ -28,23 +28,17 @@ describe('Probando DeleteButton', () => {
     //pos puede ser 0 para cancelar o 1 para confirmar
     const clickConfirm = (pos) => wrapper.find('button').at(pos).simulate('click')
 
-    /* test('Boton confirmar', async () => {
-        clickDelete();
+    test('Boton confirmar', async () => {
+       /*  clickDelete(); */
         expect(wrapper.find('DeleteButton')).toMatchSnapshot()
-        clickConfirm(1);
-        expect(wrapper.find('button').at(1)).toMatchSnapshot()
-        const found = await getEventoById(123);
-        console.log(found)
-        clickConfirm(1); 
-        const found = await getEventoById(123);
-        expect(found).toMatch('Not found')
-        wrapper.unmount(); 
-    }) */
-    /* test('Boton cancelar', async () => {
+/*         clickConfirm(1);
+        const found = await getEventoById(123); */
+    })
+    /*  test('Boton cancelar', async () => {
         expect(wrapper.find('DeleteButton')).toMatchSnapshot()
         clickDelete();
         clickConfirm(0);
         const found = await getEventoById(123);
         expect(found).toMatch({})
-    }) */
+    })  */
 })
